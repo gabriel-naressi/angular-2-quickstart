658cdf93eb976d48aefe2ab362cedb2d77364a6b
Gabriel Naressi <gabriel.fabris@synchro.com.br>
2016-09-13 18:25:54 -0300

    Entendendo o npm start

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
+//tsc -w    **equivalente ao npm run watch
+//tsc       **equivalente ao npm run build
+
 const platform = platformBrowserDynamic();
 
 platform.bootstrapModule(AppModule);
