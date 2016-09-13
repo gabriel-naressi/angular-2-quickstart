6a3c15e11f30d8054e6a0f04ce41890337176d26
Gabriel Naressi <gabriel.fabris@synchro.com.br>
2016-09-13 18:29:29 -0300

    bla

--- app/main.ts
+++ app/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 //tsc -w    **equivalente ao npm run watch
 //tsc       **equivalente ao npm run build
+// asdasdasd
 
 const platform = platformBrowserDynamic();
 
